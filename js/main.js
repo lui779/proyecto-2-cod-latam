e8155e03b8c145fa8485d8382c68209337e8639f
lui779 <garcesluis9@gmail.com>
2023-06-17 18:19:22 -0400

    Githubpages

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-// 2 - Seleccionar esa lista de nodos y detectar el click en todos los elementos
-// 3 - Cuando un elemento es clickeado , se ejecuta una funcion que realiza varias cosas:
-//  - Selecciona al elemento que ya tiene la clase active (por defecto, primero la tendrá Inicio)
-//  - Le quita a ese elmento la clase
-//  - Se la agrega al elemento que fue clickeado
-/*
-const menuItems = document.querySelectorAll('.nav-toggle');  // 1 - Seleccionar todos los items del menu
-menuItems.forEach(function(item) {
-    item.addEventListener('click', function(e) {
-        const currentItem = document.querySelector('.active');
-        currentItem.classList.remove('active');
-        e.target.classList.add('active');
-    });    
-});
-*/
 
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
